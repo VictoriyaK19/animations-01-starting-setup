3c4a0ff81edc62d5560daea8a57a55338490312f
VictoriyaK19 <kalinovavictoriya@gmail.com>
2023-08-18 13:04:37 +0300

    Using Custom CSS Class Names

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <transition name="para">
     <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraoh">Toggle Paragraph</button>
@@ -91,30 +91,17 @@ button:active {
    animation: slide-fade 0.3s ease-out forwards;
 }
 
-.v-enter-from {
- /* opacity: 0;
-  transform: translateY(-30px); */
-}
-.v-enter-active {
+
+.para-enter-active {
   animation: slide-scale 0.3s ease-out;
 }
-.v-enter-to {
- /* opacity: 1;
-  transform: translateY(0); */
-}
 
-.v-leave-from {
- /* opacity: 1;
-  transform: translateY(0); */
-}
-.v-leave-active {
+
+.para-leave-active {
  /* transition: all 0.3s ease-in; */
  animation: slide-scale 0.3s ease-out;
 }
-.v-leave-to {
- /* opacity: 0;
-  transform: translateY(30px); */
-}
+
 
 @keyframes slide-scale{
   0% {
