f36f72572bd623ea48298796c42505b95d037c1d
VictoriyaK19 <kalinovavictoriya@gmail.com>
2023-09-25 11:58:55 +0300

    Animating Lists with "transition-group"

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -1,41 +1,63 @@
 <template>
-    <ul>
-        <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
-    </ul>
-    <div>
-        <input type="text" ref="userNameInput" />
-        <button @click="addUser">Add user</button>
-    </div>
+  <transition-group tag="ul" name="user-list">
+    <li v-for="user in users" :key="user" @click="removeUser(user)">
+      {{ user }}
+    </li>
+  </transition-group>
+  <div>
+    <input type="text" ref="userNameInput" />
+    <button @click="addUser">Add user</button>
+  </div>
 </template>
 
 <script>
-export default{
-    data() {
-        return {
-            users: ['Max', 'Manu', 'Julie', 'Angela', 'Michael']
-        };
+export default {
+  data() {
+    return {
+      users: ["Max", "Manu", "Julie", "Angela", "Michael"],
+    };
+  },
+  methods: {
+    addUser() {
+      const enterUserName = this.$refs.userNameInput.value;
+      this.users.unshift(enterUserName);
     },
-    methods: {
-        addUser() {
-            const enterUserName = this.$refs.userNameInput.value;
-            this.users.unshift(enterUserName);
-        },
-        removeUser(user) {
-            this.users = this.users.filter(usr => usr !== user);
-        }
-    }
-}
+    removeUser(user) {
+      this.users = this.users.filter((usr) => usr !== user);
+    },
+  },
+};
 </script>
 
 <style scoped>
 ul {
-    list-style: none;
-    margin: 1rem 0;
-    padding: 0;
+  list-style: none;
+  margin: 1rem 0;
+  padding: 0;
 }
 li {
-    border: 1px solid #ccc;
-    padding: 1rem;
-    text-align: center;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  text-align: center;
+}
+.user-list-enter-from {
+    opacity: 0;
+    transform: translateX(-30px);
+}
+.user-list-enter-active {
+    transition: all 1s ease-out;
+}
+.user-list-enter-to,
+.user-list-leave-from {
+    opacity: 1;
+    transform: translateX(0);
+}
+
+.user-list-leave-active {
+    transition: all 1s ease-in;
+}
+.user-list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
 }
 </style>
